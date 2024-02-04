99a0403632a51f9baa803927cfc7ad6045c31d37
Mykola Herasymenko <nicksuper2000@gmail.com>
2024-02-04 09:39:09 -0500

    full project

---
+++ lib/consts.dart
@@ -0,0 +1,4 @@
+const String NEWS_API_KEY = "bb42088a0905429199348812ee79ca17";
+
+const String PLACEHOLDER_IMAGE_LINK =
+    "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png";
